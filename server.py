67d4e99397b3a0dde5c46c91a3fe909f780631b6
std67122420226-gif <std.67122420226@ubru.ac.th>
2025-12-15 12:40:00 +0700

    new edit

--- server.py
+++ server.py
@@ -1,43 +1,42 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'render_template'('index.html', title='My Home Page')
+  return render_template('index.html', title='My Home Page')
 
 @app.route('/about')
 def about():
-  return render_template('about.html'
-                         title='About Page'
-                         name='thanchanok'
-                         email='std.67122420226@ubru.ac.th'
-                         phone='080-465-8319'
-                         return render_template('about.html',
+                        title='About Page',
+                        name='thanchanok',
+                        email='std.67122420226@ubru.ac.th',
+                        phone='080-465-8319'
+                        return render_template('about.html',
                                                 title=title,
                                                 name=name,
                                                 email=email,
-                                                phone=phone))
+                                                phone=phone)
 
 @app.route('/favorite/foods')
-def favorite_foods():
+def fav_foods():
   title = 'favorite Foods Page'
   foods = ['ส้มตำ','ต้มยำกุ้งน้ำข้น','ก๋วยเตี๋ยวต้มยำ','กระเพราหมูกรอบ','หมูกะทะ']
   return render_template('fav_foods.html',
-                         foods=foods,
-                         title=title)
+                        foods=foods,
+                        title=title)
 
-@app.route('/favorite/sports')
-def sports():
+@app.route('/sports')
+def fav_sports():
   title = 'Favorite Sprots Page'
   sports = ['บอลเล่','ปิงปอง','แบดมินตัน','เปตอง']
   return render_template('fav_sports.html',
-                         title=title,
-                         sports=sports)
+                        title=title,
+                        sports=sports)
 
 @app.route('/greeting/<username>')
 def welcome(username):
   title = 'Welcome Page'
   return render_template('welcoome.html',
-                         title=title,
-                         username=username)
+                        title=title,
+                        username=username)
